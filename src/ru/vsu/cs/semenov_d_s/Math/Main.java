b82cae2bf0958644ea3c0962d347fe42efe082ed
Meetin82 <dmitriysemenov02@mail.ru>
2023-01-08 21:30:55 +0300

    Добавил в библиотеку кванторионы (пригодится для 4 таска)

--- src/ru/vsu/cs/semenov_d_s/Math/Main.java
+++ src/ru/vsu/cs/semenov_d_s/Math/Main.java
@@ -1,6 +1,11 @@
 package ru.vsu.cs.semenov_d_s.Math;
 
+import ru.vsu.cs.semenov_d_s.Math.Matrix.Matrix3;
+
 public class Main {
     public static void main(String[] args) {
+        Matrix3 m1 = Matrix3.readMatrix3();
+        Matrix3 m2 = Matrix3.readMatrix3();
+        Matrix3.printMatrix3(Matrix3.sumMatrix3(m1, m2));
     }
 }
